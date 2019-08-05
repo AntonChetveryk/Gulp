87d3689569608f81a1777ceeac1a6b91772695cc
AntonChetveryk <chetverykanton92@gmail.com>
2019-08-05 22:40:17 +0300

    Gulp

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+sass.compiler = require('node-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const imagemin = require('gulp-imagemin');
+const babel = require('gulp-babel');
+
+
+gulp.task("images", (done) => {
+    return gulp.src("./src/img/**/*")
+        .pipe(imagemin())
+        .pipe(gulp.dest("./dist/img"));
+})
+
+gulp.task("scss", (done) => {
+	return gulp.src("./src/scss/**/*.scss")
+		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions'],
+			cascade: false
+		}))
+		.pipe(gulp.dest("./dist/css"))
+});
+
+gulp.task("js", (done) => {
+    return gulp.src("./src/js/**/*.js")
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(gulp.dest("./dist/js"))
+});
